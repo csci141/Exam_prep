5959f6820b9b0da0c40eb265990cb29e9d1f17aa
Caroline Hardin <hardinc3@wwu.edu>
2024-10-07 09:32:29 -0700

    Add files via upload

---
+++ Exam0-blank.py
@@ -0,0 +1,118 @@
+#TODO 0A: name date
+
+# Exam instructions
+    # Outside assistance such as using AI or other people's assistance is not allowed; those caught will fail the class and be referred to the academic honesty board.
+    # Don't change any lines labled "DO NOT CHANGE THIS LINE"
+    # This exam has 12 tasks, each worth the same amount. Plan on about 3-4 minutes per task so you have a few minutes at the end to turn it in.
+    # If you are unable to do a task in 4 minutes, get as far as you can and then move on.
+    # Comment out lines which cause the code to crash
+    
+
+def print_intro_1():	#DO NOT CHANGE THIS LINE 
+    
+    #TODO 1A
+   
+
+
+
+    #TODO 1B
+    answer1 = ""
+   
+    
+    
+    
+    return(answer1)	#DO NOT CHANGE THIS LINE
+    
+def print_problem_2(num1, num2, num3):	#DO NOT CHANGE THIS LINE
+    
+    #TODO 2A
+   
+    answer2a = ""
+      
+    
+    
+    #TODO 2B
+    
+    answer2b = ""
+   
+    
+    
+    
+    #TODO 2C
+    
+    answer2c = ""
+
+
+
+
+    #TODO 2D
+   
+    answer2d = ""
+        
+    
+    
+    print(answer2d)	#DO NOT CHANGE THIS LINE
+    return(answer2a,answer2b, answer2c, answer2d)	#DO NOT CHANGE THIS LINE
+    
+def get_input_3(num1, num2, num3):		#DO NOT CHANGE THIS LINE
+    
+    
+    #TODO 3A
+    
+    user_answer = ""
+    
+    
+    
+    
+    #TODO 3B
+    answer3 = ""
+      
+    
+    
+    print("The correct answer is:", answer3)	#DO NOT CHANGE THIS LINE
+    return(answer3,user_answer)	#DO NOT CHANGE THIS LINE
+    
+def print_end_message_4():	#DO NOT CHANGE THIS LINE
+      
+    #TODO 4A
+   
+    
+    #TODO 4B
+    answer4b = ""
+ 
+
+
+    #TODO 4C
+    answer4c= ""
+   
+
+
+    return(answer4b, answer4c)	#DO NOT CHANGE THIS LINE
+
+
+def main(): 			#DO NOT CHANGE THIS LINE
+   
+    #To help with debugging, here are the four parts of the exam in order with example values.
+    #You can comment out or change them in any way which is useful for you.
+    #print("----Running print_intro_1...")
+    print_intro_1()
+   
+    #print("----Running print_problem_2....")
+    print_problem_2(1, 2.0, "3") 
+    
+    #print("----Running get_input_3....")
+    get_input_3(1, 2.0, "3")
+    
+    #print("----Running print_end_message_4...")
+    print_end_message_4() 
+
+
+# END OF EXAM 
+##########################################################################
+
+def get_version():			#DO NOT CHANGE THIS LINE
+    return exam_version		#DO NOT CHANGE THIS LINE
+
+if __name__ == "__main__":	#DO NOT CHANGE THIS LINE
+    #Encode in base 64 the architecture used #DO NOT CHANGE THIS LINE
+    main()					#DO NOT CHANGE THIS LINE
