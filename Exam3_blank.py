b5af58c487348423f9598b3f93d0ee2177402544
Caroline Hardin <hardinc3@wwu.edu>
2024-11-22 13:09:55 -0800

    uploaded

---
+++ Exam3_blank.py
@@ -0,0 +1,207 @@
+exam_version = 10
+#TODO 0A: your name
+
+import random     #DO NOT CHANGE THIS LINE 
+
+
+
+
+    
+
+
+
+# Exam instructions
+    # The only IDE allowed for this exam is Thonny.
+    # Outside assistance such as using AI or other people's assistance is not allowed; those caught will fail the class and be referred to the academic honesty board.
+    # Don't change any lines labeled "DO NOT CHANGE THIS LINE"
+    # This exam has 12 tasks, each worth the same amount. Plan on about 4 minutes per task so you have a few minutes at the end to turn it in.
+    # If you are unable to do a task in 4 minutes, get as far as you can and then move on.
+    # On this exam, function argument variables are part of each function definition block; you won't need system arguments for this exam.
+    # Comment out lines which cause the code to crash; code which must be hand un-crashed will receive a -1 penalty
+
+
+# Our store is pivoting to selling custom poems!
+# We'll start by letting the user ask what they want to do
+
+def user_choice_loop_7():           #DO NOT CHANGE THIS LINE          
+        
+    choice_text = "[w] write a new poem, make it [d]ramatic, make it [f]ancy, add [p]unctuation, [s]ave the poem or [q]uit:" #DO NOT CHANGE THIS LINE
+
+    #TODO 7A
+   
+    poem = "emerging epic"
+    loop_count = 0
+
+        
+    
+
+    
+    return(poem, loop_count) #DO NOT CHANGE THIS LINE
+
+def make_multiline_poem_8(num_of_stanzas, poem_as_dict, random_mode): #DO NOT CHANGE THIS LINE
+# assume:
+# 'num_of_stanzas' is a int >= 0, 
+# 'poem_as_dict' is a dictionary with keys 'starts', 'rhymes' and 'bridges'
+# 'random_mode' is a boolean
+
+    # Using randomness makes better poems but that makes testing hard. 
+    # So while testing we'll keep random_mode as False so the lists don't get shuffled.
+
+    if random_mode:   #DO NOT CHANGE THIS LINE 
+        
+        random.shuffle(poem_as_dict["starts"]) #DO NOT CHANGE THIS LINE 
+        random.shuffle(poem_as_dict["rhymes"]) #DO NOT CHANGE THIS LINE 
+        random.shuffle(poem_as_dict["bridges"]) #DO NOT CHANGE THIS LINE 
+
+    poem = "" 
+
+    #TODO 8A
+   
+    
+        
+        
+
+    return(poem) #DO NOT CHANGE THIS LINE 
+
+#TODO 10A
+
+
+def add_punctuation_13(poem_line, index_num): #DO NOT CHANGE THIS LINE
+    #assume 'poem_line' is a string and 'index' is an integer
+    #TODO 13A
+    
+    return(new_poem_line) #DO NOT CHANGE THIS LINE
+
+def make_fancy_14(poem_line):#DO NOT CHANGE THIS LINE
+    #assume 'poem_line' is a string
+             
+    #TODO 14A
+   
+
+    return(poem_line) #DO NOT CHANGE THIS LINE
+
+def make_dramatic_14(poem_line): #DO NOT CHANGE THIS LINE
+    #assume 'poem_line' is a string
+
+    #TODO 14B
+    return(poem_line)  #DO NOT CHANGE THIS LINE
+
+def make_list_from_string_15(text):#DO NOT CHANGE THIS LINE
+    #assume 'text' is a string
+
+    #TODO 15A 
+  
+    
+    text_list = []
+    
+
+    return(text_list) #DO NOT CHANGE THIS LINE
+
+def make_line_from_lists_15(starts, rhymes, bridges, num):#DO NOT CHANGE THIS LINE
+    #assume 'starts', 'rhymes', and 'bridges' are lists of strings with at least one item in them, 
+    # and 'line_num' is an integer which may be positive, negative, or 0
+    
+    #TODO 15B
+   
+
+    stanza = "" #DO NOT CHANGE THIS LINE
+
+    #TODO 15C 
+  
+     
+    return stanza  #DO NOT CHANGE THIS LINE
+
+def read_file_16(file_name):#DO NOT CHANGE THIS LINE
+    #assume 'file_name' is the name of a valid file with text in it
+    
+    #TODO 16A
+   
+    return all_text  #DO NOT CHANGE THIS LINE
+    
+def write_file_16(poem, file_name): #DO NOT CHANGE THIS LINE
+    #assume poem is a string and file_name is a string ending in ".txt"
+    
+    #TODO 16B
+   
+
+ 
+def gather_poem_parts_17(starts_list, rhymes_list, bridges_list):#DO NOT CHANGE THIS LINE
+    #assume 'starts_list', 'rhymes_list', and 'bridge_list' are lists of strings with at least 1 entry
+    #and title is a string
+
+    #TODO 17A
+  
+    poem_dict = {}
+
+    
+    
+    return(poem_dict) #DO NOT CHANGE THIS LINE
+
+
+
+def main(): 			        #DO NOT CHANGE THIS LINE
+   
+    # for testing and development purposes, here we'll call each of the functions
+    # in sequence.
+    # Nothing in Main will be graded, so you can use this to help with debugging 
+    
+    random_mode = False #for testing purposes we won't randomize the poems
+  
+    print("\n---- user_choice_loop_7()...")
+    user_choice_loop_7()
+
+    temp_start = ["start0", "start1", "the", "THE", "I've never seen a poem as lovely as a"]
+    temp_rhyme = ["rhyme0", "rhyme1", "the", "THE", "tree"]
+    temp_bridge = ["bridge0", "bridge1", "the", "THE", "amid the search for meaning deep"]
+
+    print("\n----make_multiline_poem_8...")
+    temp_dict = {"starts": temp_start, "rhymes": temp_rhyme, "bridges": temp_bridge}
+    print(make_multiline_poem_8(3, temp_dict, False))
+
+    print("\n----make_multiline_poem_8...")
+    temp_text = "start0:start1:the:THE"
+    print(make_list_from_string_15(temp_text))
+
+    print("\n----make_title_10...")
+    first = ['The', "A", "My", "Why"]
+    second = ["Dream", "Mirage", "Vision", "Not"]
+    print(make_title_10(first, second))
+
+    print("\n----add_punctuation_13...")
+    poem_line_temp = "Let the rain"
+    index = 3
+    print("returns: ", add_punctuation_13(poem_line_temp, index))
+
+    print("\n----make_dramatic_14...")
+    poem_line_temp = "can swirl rainbow tails"
+    print("returns: ", make_dramatic_14(poem_line_temp))
+
+    print("\n----make_fancy_14...")
+    poem_line_temp = "Let the rain"
+    print("returns: ", make_fancy_14(poem_line_temp))
+
+    print("\n----make_line_from_lists_15...")
+    poem_line = make_line_from_lists_15(temp_start, temp_rhyme, temp_bridge, 3)
+    print("returns: ", poem_line)
+
+    print("\n----read_file_16...")
+    print("returns: ", read_file_16("bridges_"+str(exam_version)+".txt"))
+
+    print("\n----write_file_16...")
+    temp_poem = "Quoth the Raven,\n 'Nevermore.'"
+    write_file_16(temp_poem, "Exam3_practice_write.txt")
+
+    print("\n----gather_poem_parts_17...")
+    print("returns: ", gather_poem_parts_17(temp_start, temp_rhyme, temp_bridge))
+
+  
+
+# END OF EXAM 
+##########################################################################
+
+def get_version():			#DO NOT CHANGE THIS LINE
+    return exam_version		#DO NOT CHANGE THIS LINE
+
+if __name__ == "__main__":	#DO NOT CHANGE THIS LINE
+    #Encode in base 64 the architecture used #DO NOT CHANGE THIS LINE
+    main()					#DO NOT CHANGE THIS LINE
