6ebe1814217c25960f5ac6bdeb8e4acd420ce165
Caroline Hardin <hardinc3@wwu.edu>
2024-12-14 19:29:02 -0800

    Add files via upload

---
+++ Meilin_Scott_Exam_Prep_Questions.py
@@ -0,0 +1,48 @@
+# Name: Meilin Scott
+# Date: 12/5/2024
+# Description: Exam 1 practice questions
+
+# Welome to Chen's Jewelry, a luxury jewelry store in Los, Angeles, CA!
+
+
+def hello_world_0(): #DO NOT CHANGE THIS LINE
+
+    #TODO 0A: # Day 0: Hello World
+    #Print a welcome message that says "Welcome to Chen's Jewelery!".
+
+    pass
+
+def enter_hours_1(): #DO NOT CHANGE THIS LINE
+    
+    #TODO 1A: # Day 1: Computers, Algorithms, Pseudocode
+    # You are an employer at Chen's Jewelry and need your employees to enter their hours on a timesheet.
+    # a. Ask the user  "Input hours: " and save their response in the variable 'hours'.
+    
+    hours = ""
+    
+    #TODO 1B: Print "You worked x hours today." where x is replace by the hours they entered.
+    
+    
+    return hours #DO NOT CHANGE THIS LINE
+
+def main():
+    
+    # main() is for testing and development purposes.
+    # I've provided some basic code to help with testing,
+    # however you will need to add extra test cases
+    # and potentially debug the provided code.
+    #
+    # Nothing in main() will be graded, so you can
+    # change it in any way you wish to help with debugging 
+    
+    print("\n----hello_world_0...")
+    hello_world_0()
+    
+    print("\n----enter_hours_1...")
+    print("returns hours: ", enter_hours_1())
+    
+
+
+if __name__ == "__main__":	#DO NOT CHANGE THIS LINE
+  
+    main()					#DO NOT CHANGE THIS LINE
