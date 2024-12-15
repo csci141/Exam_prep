6f2b9fcddf927f255a16c1e7152a21707b154999
Caroline Hardin <hardinc3@wwu.edu>
2024-12-14 19:07:26 -0800

    Add files via upload

---
+++ additional_exam_questions.py
@@ -0,0 +1,85 @@
+#Author: Ash Summers
+#Date Dec 5th
+#Description: Proposed Final Exam Questions
+
+
+
+#The story for this code is a Diamond Store
+
+def welcome_message():
+    #TODO 1
+    #Welcome the user to your Diamond Store by PRINTING "Welcome to my Diamond Store!" (without the quotations)
+    #It's recommended that you copy/paste the phrase inside the quotations to prevent typos
+    #This function DOES NOT return anything
+    
+
+def change_type(num1, num2): 
+    #TODO 2
+    # num1 and num2 are integers
+    #Convert num1 and num2 into floats and RETURN them
+    # num1 and num2 are set to 0 as a placeholder so the code doesn't crash
+    
+    num1 = 0
+    num2 = 0
+    
+def calculate_diamond_price(per_carat, weight):
+    #TODO 3
+    #You need to calculate the price of a diamond for the diamond store
+    #per_carat is the price per carat and weight is the weight of the diamond in carats
+    #Store the value in the variable price and RETURN price
+    #price is calculated by multiplying the price_carat by the weight
+    #price is set to 0 so the code doesn't crash
+    
+    price = 0
+    
+def barcodes():
+    #TODO 4
+    #Your store needs to have a barcode for the diamond
+    #Convert 15 into binary and store the binary number in the variable barcode
+    #RETURN barcode
+    #barcode is set to 0 so the code doesn't crash
+    
+    barcode = 0
+
+def price_per_carat(diamond_type):
+
+    #You need to determine the price per carat of a diamond and store it in the variable per_carat
+    #Assume diamond_type is a string
+    #per_carat is set to 0 so the code doesn't crash
+    
+    per_carat = 0
+    
+    #TODO 56 A
+    #If the diamond_type is "yellow", the price per carat should be set to 4000
+    
+    #TODO 56 B
+    #Else if the diamond_type is "black", the price per carat should be set to 3000
+    
+    #TODO 56 C
+    #Else if the diamond_type is "colorless", the price per carat should be set to 5000
+    
+    
+    return(per_carat) #DO NOT CHANGE THIS LINE
+
+def thank_you_message(message):
+    
+    #TODO 12
+    #You need to make the thank you message for the store more exciting
+    #Make message uppercase and store it in the variable exciting_message
+    #Asumme message is a string
+    #exciting_message is set to "" so the code doesn't crash
+    
+    
+    exciting_message = ""
+    
+    return(exciting_message) #DO NOT CHANGE THIS LINE
+    
+    
+
+
+#For loops
+# end= and sep=
+# Turtles
+#Write a function
+#Scope errror
+#Tuples
